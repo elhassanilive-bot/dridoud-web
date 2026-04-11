680a8a0758fc85d6d84756c3e94719d8a023313a
Said Elhassani <elhassanilive@gmail.com>
2026-04-11 21:16:29 +0100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,11 +8,11 @@ export const metadata = {
   applicationName: site.nameEn,
   icons: {
     icon: [
-      { url: "/favicon.ico?v=2", type: "image/png" },
-      { url: "/icon.png?v=2", type: "image/png" },
+      { url: "/favicon.ico?v=3", type: "image/png" },
+      { url: "/icon.png?v=3", type: "image/png" },
     ],
-    shortcut: "/favicon.ico?v=2",
-    apple: "/apple-icon.png?v=2",
+    shortcut: "/favicon.ico?v=3",
+    apple: "/apple-icon.png?v=3",
   },
   title: {
     default: `${site.name} | ${site.nameEn}`,
